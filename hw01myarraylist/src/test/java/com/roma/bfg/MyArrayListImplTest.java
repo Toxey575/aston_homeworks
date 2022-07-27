f1657aa4f1858283e1530bfda5a8e77a5395e20d
Roma1338 <rd.golovachev@gmail.com>
2022-07-28 00:26:50 +0300

    myArrayList no tests

---
+++ hw01myarraylist/src/test/java/com/roma/bfg/MyArrayListImplTest.java
@@ -0,0 +1,41 @@
+package com.roma.bfg;
+
+import org.junit.jupiter.api.Test;
+
+class MyArrayListImplTest {
+
+    private final MyArrayListImpl<Integer> myIntegerArrayList = new MyArrayListImpl<>();
+    private final MyArrayListImpl<Integer> myStringArrayList = new MyArrayListImpl<>();
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void addAll() {
+    }
+
+    @Test
+    void clear() {
+    }
+
+    @Test
+    void get() {
+    }
+
+    @Test
+    void set() {
+    }
+
+    @Test
+    void indexOf() {
+    }
+
+    @Test
+    void remove() {
+    }
+
+    @Test
+    void testRemove() {
+    }
+}
